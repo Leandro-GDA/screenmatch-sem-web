d425d9a2e55b9873956d6ef6ada30694e4bd6b09
Leandro-GDA <leandro.gonzales277@gmail.com>
2024-03-08 15:48:02 -0300

    Adding new features

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private final String ENDERECO = "\"https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=2d98fb97";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var consumoApi = new ConsumoApi();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+    }
+}
